fb62a83a86158834f8a9da85afe5ba0d52b7553a
Klavdius <39763793+Klavdius@users.noreply.github.com>
2024-01-26 14:32:56 +0300

    Job is done

--- carNumber.go
+++ carNumber.go
@@ -9,107 +9,100 @@ import (
 
 func main() {
 	var amountRepit int
-	errFor4 := 0
-	errFor5 := 0
-	goodFourNumberCar := []string{}
-	goodFiveNumberCar := []string{}
 	fmt.Scanln(&amountRepit)
 	scaner := bufio.NewScanner(os.Stdin)
 	for i := 0; i < amountRepit; i++ {
 		scaner.Scan()
 		inputString := scaner.Text()
-		goodFiveNumberCar, errFor5 = ChekingLineFiveSimvol(inputString)
-		goodFourNumberCar, errFor4 = ChekingLineFourSimvol(inputString)
+		ChekingLengInputLine(inputString)
+	}
+}
 
-		if errFor4 == 0 {
-			for _, v := range goodFourNumberCar {
-				fmt.Print(v + " ")
-			}
-		} else if errFor5 == 0 {
-			for _, v := range goodFiveNumberCar {
-				fmt.Print(v + " ")
-			}
+func ChekingLengInputLine(inputString string) {
+	lengInputString := len(inputString)
+	if lengInputString >= 4 {
+		ChekingInputLine(inputString)
+	} else {
+		fmt.Println("-")
+	}
+}
+
+func ChekingInputLine(inputString string) {
+	var goodCarNumber = []string{}
+	for len(inputString) > 0 {
+		if len(inputString) < 4 {
+			goodCarNumber = append(goodCarNumber, "-")
+			break
 		} else {
-			fmt.Println("-")
-		}
-	}
-}
-
-func ChekingLineFourSimvol(inputString string) ([]string, int) {
-	var (
-		line            string
-		lengInputString int
-		detail          int
-	)
-	goodCarNumbers := []string{}
-	err := 0
-	lengInputString = len(inputString)
-	if lengInputString%4 == 0 {
-		for i := 0; i <= lengInputString; i = i + 3 {
-			line = inputString[:4]
-			inputString = inputString[4:]
-			detail = DetailFourLine(line)
-			if detail == 1 {
-				goodCarNumbers = append(goodCarNumbers, line)
+			lineForTest := inputString[:4]
+			applicant := DetailFourLine(lineForTest)
+			if applicant == "-" {
+				if len(inputString) < 5 {
+					goodCarNumber = append(goodCarNumber, "-")
+					break
+				} else {
+					lineForTest = inputString[:5]
+					applicant = DetailFiveLine(lineForTest)
+					if applicant == "-" {
+						goodCarNumber = append(goodCarNumber, "-")
+						break
+					} else {
+						goodCarNumber = append(goodCarNumber, applicant)
+						inputString = inputString[5:]
+					}
+				}
 			} else {
-				err = 1
+				goodCarNumber = append(goodCarNumber, applicant)
+				inputString = inputString[4:]
 			}
 		}
-	} else {
-		err = 1
 	}
 
-	return goodCarNumbers, err
+	if len(inputString) != 0 {
+		fmt.Println("-")
+	} else {
+		CheckSliceGoodCarNumber(goodCarNumber)
+	}
 }
-
-func ChekingLineFiveSimvol(inputString string) ([]string, int) {
-	var (
-		line            string
-		lengInputString int
-		detail          int
-	)
-	goodCarNumbers := []string{}
-	err := 0
-	lengInputString = len(inputString)
-	if lengInputString%5 == 0 {
-		for i := 0; i <= lengInputString; i = i + 4 {
-			line = inputString[:5]
-			inputString = inputString[5:]
-			detail = DetailFiveLine(line)
-			if detail == 1 {
-				goodCarNumbers = append(goodCarNumbers, line)
-			} else {
-				err = 1
-			}
+func CheckSliceGoodCarNumber(inputDataInSlice []string) {
+	errorInSlice := 0
+	for _, v := range inputDataInSlice {
+		if v == "-" {
+			errorInSlice = 1
 		}
+	}
+	if errorInSlice > 0 {
+		fmt.Println("-")
 	} else {
-		err = 1
+		for _, v := range inputDataInSlice {
+			fmt.Print(v + " ")
+		}
+		fmt.Print("\n")
 	}
 
-	return goodCarNumbers, err
 }
 
-func DetailFourLine(line string) int {
-	var result int
+func DetailFourLine(line string) string {
+	var result string
 	firstIndexNumber := strings.IndexAny(line, "0123456789")
 	lastIndexNumber := strings.LastIndexAny(line, "0123456789")
 	if firstIndexNumber == lastIndexNumber && firstIndexNumber == 1 {
-		result = 1
+		result = line
 	} else {
-		result = 0
+		result = "-"
 	}
 
 	return result
 }
 
-func DetailFiveLine(line string) int {
-	var result int
+func DetailFiveLine(line string) string {
+	var result string
 	firstIndexNumber := strings.IndexAny(line, "0123456789")
 	lastIndexNumber := strings.LastIndexAny(line, "0123456789")
 	if firstIndexNumber == 1 && lastIndexNumber == 2 {
-		result = 1
+		result = line
 	} else {
-		result = 0
+		result = "-"
 	}
 
 	return result
